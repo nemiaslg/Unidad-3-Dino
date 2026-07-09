fc674f5a2a367ddf8ef72636676789803454ae46
nemiaslg <nemiaslopez14@gmail.com>
2026-07-09 01:40:35 -0600

    agrega clase HiloDinosaurio

---
+++ HiloDinosaurio.java
@@ -0,0 +1,73 @@
+
+/**
+ * Write a description of class HiloDinosaurio here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HiloDinosaurio extends Thread
+{
+    private Juego juego;
+    private PanelJuego panel;
+
+    public HiloDinosaurio(Juego juego, PanelJuego panel)
+    {
+        this.juego = juego;
+        this.panel = panel;
+    }
+
+    @Override
+    public void run()
+    {
+        while(!juego.isGameOver())
+        {
+            if(juego.isSaltando())
+            {
+                // Subir
+                while(juego.getDinoY() > 170)
+                {
+                    juego.setDinoY(juego.getDinoY() - 5);
+
+                    panel.repaint();
+
+                    try
+                    {
+                        Thread.sleep(15);
+                    }
+                    catch(InterruptedException e)
+                    {
+                        return;
+                    }
+                }
+
+                // Bajar
+                while(juego.getDinoY() < 250)
+                {
+                    juego.setDinoY(juego.getDinoY() + 5);
+
+                    panel.repaint();
+
+                    try
+                    {
+                        Thread.sleep(15);
+                    }
+                    catch(InterruptedException e)
+                    {
+                        return;
+                    }
+                }
+
+                juego.setSaltando(false);
+            }
+
+            try
+            {
+                Thread.sleep(10);
+            }
+            catch(InterruptedException e)
+            {
+                return;
+            }
+        }
+    }
+}
